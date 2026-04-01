a02cc855dd9bac589f84c2fe10739307a98a4641
ChanderG <mail@chandergovind.org>
2026-04-01 10:56:17 +0530

    add reference impl and compare results

--- part3/main.cpp
+++ part3/main.cpp
@@ -2,6 +2,8 @@
 #include <cstdint>
 #include <memory>
 #include <random>
+#include <cassert>
+#include <cstring>
 
 using namespace std;
 
@@ -59,6 +61,49 @@ void initRandomTensor2F(Tensor2F *t) {
   }
 }
 
+float get(Tensor2F *t, int i, int j) {
+  // Not the most efficient, but ok for now
+  float* base = (float*)((uintptr_t*)t->aligned + t->offset);
+  return base[i*t->strides[0] + j*t->strides[1]];
+}
+
+void set(Tensor2F *t, int i, int j, float val) {
+  // Not the most efficient, but ok for now
+  float* base = (float*)((uintptr_t*)t->aligned + t->offset);
+  base[i*t->strides[0] + j*t->strides[1]] = val;
+}
+
+Tensor2F calcReference(Tensor2F *a, Tensor2F* b) {
+  auto x = a->sizes[0];
+  auto y = a->sizes[1];
+
+  auto y_ = b->sizes[0];
+  auto z = b->sizes[1];
+
+  assert(y == y_ && "Matrices dimensions should match up for matmul");
+
+  auto c = createTensor2F(x, z);
+
+  // naive implementation - can take a lot of time
+  for (int i = 0; i < x; i++) {
+    for (int k = 0; k < z; k++) {
+      // calc result for c[i, k]
+      float acc = 0.0;
+      for (int j = 0; j < y; j++) {
+        acc += get(a, i, j)*get(b, j, k);
+      }
+
+      // let's do the fused relu here
+      if (acc < 0) {
+        acc = 0;
+      }
+      set(&c, i, k, acc);
+    }
+  }
+
+  return c;
+}
+
 int main() {
   rng.seed(42);
 
@@ -69,8 +114,18 @@ int main() {
   initRandomTensor2F(&in1);
   initRandomTensor2F(&in2);
 
+  // calc naive reference value
+  auto ref = calcReference(&in1, &in2);
+
   _mlir_ciface_myfunc(&res, &in1, &in2);
 
   cout << "Tensor is: " << res.sizes[0] << "x" << res.sizes[1] << endl;
   cout << "Strides are: " << res.strides[0] << "x" << res.strides[1] << endl;
+
+  auto cmp = memcmp(res.aligned, ref.aligned, ref.sizes[0]*ref.sizes[1]*sizeof(float));
+  if (cmp == 0) {
+    cout << "MLIR version matches reference!" << endl;
+  } else {
+    cout << "result != reference !!!" << endl;
+  }
 }
